72cef2a212078385f3b87c0a12b2493d86917766
Atilano <atilanof@hotmail.com>
2021-11-26 09:02:14 +0100

    Segunda subida

---
+++ src/main/java/icai/dtc/dominio/Personaje.java
@@ -0,0 +1,5 @@
+package icai.dtc.dominio;
+
+public interface Personaje {
+
+}
--- src/main/java/icai/dtc/patrones/AbstractFactoryPersonaje.java
+++ src/main/java/icai/dtc/patrones/AbstractFactoryPersonaje.java
@@ -1,5 +1,7 @@
 package icai.dtc.patrones;
 
+import icai.dtc.dominio.Personaje;
+
 public abstract class AbstractFactoryPersonaje {
     public abstract Personaje getPersonaje(String personaje);
 
